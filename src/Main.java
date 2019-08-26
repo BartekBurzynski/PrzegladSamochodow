b25973726200ccc781b0d56fc244a3d942d12fae
BartekBurzynski <bartlomiej.burzynski@wp.pl>
2019-08-26 19:08:03 +0200

    PrzegladSamochodow.

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,10 @@ import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
-   private static final int programEnd = 0;
-   private static final int addNewCar = 1;
-   private static final int getGar = 2;
+    private static final Scanner input = new Scanner(System.in);
+    private static final int programEnd = 0;
+    private static final int addNewCar = 1;
+    private static final int getGar = 2;
 
     public static void main(String[] args) {
         carQueue();
@@ -13,50 +14,46 @@ public class Main {
     }
 
     public static void carQueue() {
-        Scanner input1 = new Scanner(System.in);
+
         Queue<Car> carQueue = new LinkedList<>();
         int decision;
 
-
         do {
             System.out.println("co chcesz zrobić ?");
-            decision = input1.nextInt();
-            input1.nextLine();
-
-            if (decision == addNewCar) {
-                Car car = addCar();
-                carQueue.offer(car);
-            } else if (decision == getGar) {
-                carQueue.poll();
-            } else if (decision == programEnd) {
-                System.out.println("Do zobaczenia");
-                break;
+            decision = input.nextInt();
+            input.nextLine();
+            switch (decision) {
+                case addNewCar:
+                    Car car = addCar();
+                    carQueue.offer(car);
+                    break;
+                case getGar:
+                    if (carQueue.isEmpty()) {
+                        System.out.println("Brak samochodów");
+                    } else carQueue.poll();
+                    break;
+                case programEnd:
+                    System.out.println("Do zobaczenia");
+                    break;
+                default:
             }
         } while (decision != programEnd);
     }
 
-
     private static Car addCar() {
-        String type;
-        String marka;
-        String model;
-        int year;
-        int kilometers;
-        int vin;
 
-        Scanner input = new Scanner(System.in);
         System.out.println("Podaj typ");
-        type = input.nextLine();
+        String type = input.nextLine();
         System.out.println("Podaj marke");
-        marka = input.nextLine();
+        String marka = input.nextLine();
         System.out.println("Podaj model");
-        model = input.nextLine();
+        String model = input.nextLine();
         System.out.println("Podaj rok");
-        year = input.nextInt();
+        int year = input.nextInt();
         System.out.println("Podaj przebieh");
-        kilometers = input.nextInt();
+        int kilometers = input.nextInt();
         System.out.println("Podaj vin");
-        vin = input.nextInt();
+        int vin = input.nextInt();
         Car car = new Car(type, marka, model, year, kilometers, vin);
         return car;
     }
